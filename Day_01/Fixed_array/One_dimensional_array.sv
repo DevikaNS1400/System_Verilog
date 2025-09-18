b650c82321c2c011fc02c3698b3c52f4796a925a
N S Devika <nsdevika2000@gmail.com>
2025-09-18 17:03:54 +0530

    Create One_dimensional_array.sv

---
+++ Day_01/Fixed_array/One_dimensional_array.sv
@@ -0,0 +1,19 @@
+module mod_1d_arr;
+  int arr[5];
+  int arr2[2:0];
+  initial begin 
+    foreach(arr[i])begin
+      
+      arr[i]=$random;
+      $display("arr[%0d]=%d",i,arr[i]);
+    end
+  end
+endmodule
+
+/*Output
+arr[0]= 303379748
+arr[1]=-1064739199
+arr[2]=-2071669239
+arr[3]=-1309649309
+arr[4]= 112818957
+*/
