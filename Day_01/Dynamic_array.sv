e6e98a9a8814c1140f125b319abd00a1149aa93c
N S Devika <nsdevika2000@gmail.com>
2025-09-22 07:01:59 +0530

    Create Dynamic_array.sv

---
+++ Day_01/Dynamic_array.sv
@@ -0,0 +1,22 @@
+module dyn_arr;
+  int dyn[];
+  initial begin
+    dyn=new[5];
+    dyn[0]=4;
+    dyn[1]=7;
+    dyn[2]=2;
+    dyn[3]=9;
+    dyn[4]=10;
+    foreach(dyn[i])
+      $display("dyn[%0d]=%0d",i,dyn[i]);
+  end
+endmodule
+
+/*
+Output
+dyn[0]=4
+dyn[1]=7
+dyn[2]=2
+dyn[3]=9
+dyn[4]=10
+*/
