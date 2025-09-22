58ea1afc00d8b3279fe751514ffb55ec9516c03f
N S Devika <nsdevika2000@gmail.com>
2025-09-22 07:12:11 +0530

    Update Dynamic_array_2.sv

--- Day_01/Dynamic_array/Dynamic_array_2.sv
+++ Day_01/Dynamic_array/Dynamic_array_2.sv
@@ -1,1 +1,38 @@
+module dyn_array;
+  int dyn[];
+  initial begin
+    dyn=new[5];
+    dyn[0]=4;
+    dyn[1]=7;
+    dyn[2]=2;
+    dyn[3]=9;
+    dyn[4]=10;
+    
+    foreach(dyn[i])
+      $display("dyn[%0d]=%0d",i,dyn[i]);
+    
+    $display("Size of dyn=%0d",dyn.size());
+    
+    dyn=new[6](dyn);
+    $display("Size of dyn=%0d",dyn.size());
+    foreach(dyn[i])
+      $display("dyn[%0d]=%0d",i,dyn[i]);
+    
+  end
+endmodule
 
+/*Output
+dyn[0]=4
+dyn[1]=7
+dyn[2]=2
+dyn[3]=9
+dyn[4]=10
+Size of dyn=5
+Size of dyn=6
+dyn[0]=4
+dyn[1]=7
+dyn[2]=2
+dyn[3]=9
+dyn[4]=10
+dyn[5]=0
+*/
