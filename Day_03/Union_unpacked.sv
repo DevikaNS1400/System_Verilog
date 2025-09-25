b67c44001b6c5f67bf74f5d28e450f1dbe7c7942
N S Devika <nsdevika2000@gmail.com>
2025-09-25 12:23:06 +0530

    Create Union_unpacked.sv

---
+++ Day_03/Union_unpacked.sv
@@ -0,0 +1,32 @@
+module union_unpacked;
+  logic [31:0]x;
+  
+  typedef union{
+    int a;
+    byte b;
+    bit[15:0]c;
+  }data;
+  
+  data d1;
+  
+  initial begin
+    d1.a=32'hffff_ffff;
+    x=d1.b;
+    $display("x=%0h",x);
+    
+    d1.b=8'h01;
+    x=d1.a;
+    $display("x=%0h",x);
+    
+    d1.c=16'h1010;
+    x=d1.a;
+    $display("x=%0h",x);
+  end
+endmodule
+
+/*
+OUTPUT
+x=ffffffff
+x=ffffff01
+x=ffff1010
+*/
