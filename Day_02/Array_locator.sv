96cb2a6f2e8d22fb2641cbaabb8eb6a2bf5f024c
N S Devika <nsdevika2000@gmail.com>
2025-09-25 10:15:18 +0530

    Update Array_locator.sv

--- Day_02/Array_locator.sv
+++ Day_02/Array_locator.sv
@@ -1,38 +1,3 @@
-// Code your design here
-/*
-module array_Reduction;
-  int A[4]='{3,15,1,9};
-  logic [7:0]B[2][2]='{'{1,2},'{3,4}};
-  int y;
-  
-  initial begin
-    y=A.sum;
-    $display("A.sum=%0d",y);
-    
-    y=A.sum	with(item+1);
-    $display("A.sum=%0d",y);
-    
-    y=B.sum with (item.sum with(item));
-    $display("B.sum=%0d",y);
-     
-    y=A.xor;
-    $display("A.xor=%0h",y);     
-             
-    y=B.xor(item) with(item.xor);
-    $display("B.xor=%0d",y);  
-             
-    y=A.product;
-    $display("A.product=%0d",y); 
-             
-    y=A.and;
-    $display("A.and=%0d",y);
-             
-    y=A.or;
-    $display("A.or=%0d",y);         
-  end
-  endmodule
-  */
-
 module array_locator;
   int q[$]='{1,3,5,7};
   int tq[$];
